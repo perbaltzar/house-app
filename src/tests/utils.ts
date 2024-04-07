ff10300a4fe0a24fcbbca42fc6dea16b0a767ad7
perbaltzar <per.baltzar@hiberworld.com>
2024-04-07 10:44:55 +0200

    remove svg and comments

--- src/tests/utils.ts
+++ src/tests/utils.ts
@@ -1,7 +1,8 @@
-import { ReactNode } from 'react'
-import { render as testRender } from '@testing-library/react'
+import { ReactNode } from 'react';
+import { render as testRender } from '@testing-library/react';
 
+// I wrapped the render function to add some custom logic later
 export const render = (Node: ReactNode) => {
-  return testRender(Node)
-}
-export * from '@testing-library/react'
+  return testRender(Node);
+};
+export * from '@testing-library/react';
